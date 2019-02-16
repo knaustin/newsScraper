7823eb131d6f255b5dfcebebf79f19c7ef3eb8d2
jittythekitty <jittythekitty@gmail.com>
2019-02-16 18:32:18 -0500

    second commit

--- controllers/scraper_controller.js
+++ controllers/scraper_controller.js
@@ -1,75 +1,169 @@
-var express = require('express');
-var request = require('request');
-var cheerio = require('cheerio');
-var mongoose = require('mongoose');
+var express = require("express");
+var router = express.Router();
+var request = require("request");
+var cheerio = require("cheerio");
+var mongoose = require("mongoose");
 
+mongoose.Promise = Promise;
 
 var Note = require("../models/Note.js");
 var Article = require("../models/Article.js");
 
-app.get('/', function (req, res) {
-    res.render("index");
+router.get("/", function(req, res) {
+  res.render("index");
 });
 
+router.get("/savedarticles", function(req, res) {
 
-app.get('/scrape', function (req, res) {
-    axios.get('http://www.nytimes.com/').then(function (response) {
-        var $ = cheerio.load(response.data);
+  Article.find({}, function(error, doc) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      var hbsArticleObject = {
+        articles: doc
+      };
 
-        $('article h2').each(function (i, element) {
-            var result = {};
+      res.render("savedarticles", hbsArticleObject);
+    }
+  });
+});
 
-            result.title = $(this)
-                .children('span')
-                .text();
-            result.link = $(this)
-                .children('a')
-                .attr('href');
+router.post("/scrape", function(req, res) {
 
-            db.Article.create(result)
-                .then(function (dbArticle) {
-                    console.log(dbArticle);
-                })
-                .catch(function (err) {
-                    console.log(err);
-                });
-        });
-        res.send("Scrape Complete");
+  request("http://www.nytimes.com/", function(error, response, html) {
+    var $ = cheerio.load(html);
+
+    var scrapedArticles = {};
+    $("article h2").each(function(i, element) {
+
+      var result = {};
+
+      result.title = $(this)
+      .children("a")
+      .text();
+
+      console.log(result.title);
+      
+      result.link = $(this)
+      .children("a")
+      .attr("href");
+
+      scrapedArticles[i] = result;
+
     });
+
+    console.log(crapedArticles);
+
+    var hbsArticleObject = {
+        articles: scrapedArticles
+    };
+
+    res.render("index", hbsArticleObject);
+
+  });
 });
 
-app.get('/articles', function(req, res) {
-    db.Article.find({})
-    .then(function(dbArticle) {
-        res.json(dbArticle);
-    })
-    .catch(function(err) {
-        res.json(err);
-    });
+router.post("/save", function(req, res) {
+
+  console.log("This is the title: " + req.body.title);
+
+  var newArticleObject = {};
+
+  newArticleObject.title = req.body.title;
+
+  newArticleObject.link = req.body.link;
+
+  var entry = new Article(newArticleObject);
+
+  console.log("We can save the article: " + entry);
+
+
+  entry.save(function(err, doc) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log(doc);
+    }
+  });
+
+  res.redirect("/savedarticles");
+
 });
 
-app.get('/articles/:id', function(req, res) {
-    db.Article.findOne({_id: req.params.id })
-    .populate("note")
-    .then(function(dbArticle) {
-        res.json(dbArticle);
-    })
-    .catch(function(err) {
-        res.json(err);
-    });
+router.get("/delete/:id", function(req, res) {
+
+  console.log("ID is getting read for delete" + req.params.id);
+
+  console.log("Able to activate delete function.");
+
+  Article.findOneAndRemove({"_id": req.params.id}, function (err, offer) {
+    if (err) {
+      console.log("Not able to delete:" + err);
+    } else {
+      console.log("Able to delete, Yay");
+    }
+    res.redirect("/savedarticles");
+  });
 });
 
-app.post('articles/:id', function(req, res) {
-    db.Note.create(req.body)
-    .then(function(dbNote) {
-        return db.Article.findOneAndUpdate({ _id: req.params.id}, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-        res.json(dbArticle);
-    })
-    .catch(function(err) {
-        res.json(err);
-    });
+router.get("/notes/:id", function(req, res) {
+
+  console.log("ID is getting read for delete" + req.params.id);
+
+  console.log("Able to activate delete function.");
+
+  Note.findOneAndRemove({"_id": req.params.id}, function (err, doc) {
+    if (err) {
+      console.log("Not able to delete:" + err);
+    } else {
+      console.log("Able to delete, Yay");
+    }
+    res.send(doc);
+  });
 });
 
+router.get("/articles/:id", function(req, res) {
+
+  console.log("ID is getting read" + req.params.id);
+
+  Article.findOne({"_id": req.params.id})
+
+  .populate('notes')
+
+  .exec(function(err, doc) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log(doc);
+      res.json(doc);
+    }
+  });
+});
+
+router.post("/articles/:id", function(req, res) {
+
+  var newNote = new Note(req.body);
+  newNote.save(function(error, doc) {
+    if (error) {
+      console.log(error);
+    } 
+    else {
+      Article.findOneAndUpdate({ "_id": req.params.id }, {$push: {notes: doc._id}}, {new: true, upsert: true})
+
+      .populate('notes')
+
+      .exec(function (err, doc) {
+        if (err) {
+          console.log("Cannot find article.");
+        } else {
+          console.log("On note save we are getting notes? " + doc.notes);
+          res.send(doc);
+        }
+      });
+    }
+  });
+});
 module.exports = router;
